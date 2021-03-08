ebedf09bf59730e250961958020ea19def2c06d5
salmanhumayl <salman137@hotmail.com>
2021-03-08 08:58:30 +0400

    8feb

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -36,33 +36,14 @@ export class HeaderComponent implements OnInit,AfterViewInit {
               }
 
    ngAfterViewInit(){
-    //this.AddProduct();
-   // this.addCrowsel();
+
 
   }
 
 
 
 
-  addCrowsel()
-  {
 
-  var $slider = $('.slider_area');
-      if($slider.length > 0){
-
-          $slider.owlCarousel({
-              animateOut: 'fadeOut',
-              loop: true,
-              nav: false,
-              autoplay: false,
-              autoplayTimeout: 8000,
-              items: 1,
-              dots:true,
-          });
-
-  }
-
-  }
 
 
   ngOnInit(): void {
